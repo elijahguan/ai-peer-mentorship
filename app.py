ea50f75b242e3530065154abc6400ecf6caa27d2
Elijah Guan <guan5138@gmail.com>
2026-04-14 12:18:18 -0700

    almost

--- app.py
+++ app.py
@@ -18,6 +18,10 @@ def load_data():
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/mentor")
+def home():
+    return render_template("mentor.html")
 @app.route('/match', methods=['POST'])
 def match_student():
     data = request.json
@@ -54,5 +58,5 @@ def add_mentor():
     return jsonify({"status": "mentor added"})
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
 
